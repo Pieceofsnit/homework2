b46b6e2a60b132d7ad083d12fcc302f0db438a89
Pieceofsnit <102411114+Pieceofsnit@users.noreply.github.com>
2023-12-20 12:56:02 +0500

    Add files via upload

---
+++ Scripts/Spawner.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private List<AppleCoin> _appleCoins;
+    [SerializeField] private List<Transform> _spawnPoints;
+
+    private int _delay = 2;
+
+    private void Start()
+    {
+        _spawnPoints = new List<Transform>(_spawnPoints);
+        StartCoroutine(SpawnAppleCoins());
+    }
+
+    private IEnumerator SpawnAppleCoins()
+    {
+        var spawn = Random.Range(0, _spawnPoints.Count);
+        var waitForSeconds = new WaitForSeconds(_delay);
+
+        if(_appleCoins.Count <= _spawnPoints.Count)
+        { 
+            for (int i = 0; i < _appleCoins.Count; i++)
+            {
+                Instantiate(_appleCoins[i], _spawnPoints[spawn].transform.position, Quaternion.identity);
+                _spawnPoints.RemoveAt(spawn);
+                yield return waitForSeconds;
+            }
+        }
+    }
+}
